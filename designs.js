dde77d11dcdacb59caabfb29cd958432ef2bc4ea
szilviaszimon <77399095+szilviaszimon@users.noreply.github.com>
2021-03-01 16:53:54 +0000

    Add files via upload

--- designs.js
+++ designs.js
@@ -1,5 +1,4 @@
 //Select color input
-
 const color = document.getElementById("colorPicker");
 
 //Select size input
@@ -7,29 +6,32 @@ const size = document.getElementById("sizePicker");
 const table = document.getElementById("pixelCanvas");
 
 //When size is submitted by the user, call makeGrid()
-sizePicker.addEventListener('click', function(event) {
-    event.preventDefault();
-    const height = document.querySelector("#inputHeight").value;
-    const width = document.querySelector("#inputWidth").value;
-    table.innerHTML = ""
-    makeGrid(height, width);
+sizePicker.addEventListener("click", function (event) {
+  event.preventDefault();
+  const height = document.querySelector("#inputHeight").value;
+  const width = document.querySelector("#inputWidth").value;
+  table.innerHTML = "";
+  makeGrid(height, width);
 });
 
-function makeGrid (height, width) {
-    console.log(height);
-    console.log(width);
-
+function makeGrid(height, width) {
+  console.log(height);
+  console.log(width);
+  
 //Create the rows
-
-    for (var x=0; x<height; x++) {
-        var row = document.createElement("tr");
-        for (y=0; y<width; y++) {
-            var cell = document.createElement("td");
-            row.appendChild(cell);
-            cell.addEventListener("click", function (event) {
-                cell.style.backgroundColor = color.value;
-            });
+  for (var x=0; x<height; x++) {
+    var row = document.createElement("tr");
+    for (y=0; y<width; y++) {
+      var cell = document.createElement("td");
+      row.appendChild(cell);
     }
     table.appendChild(row);
   }
 }
+
+//Add the color
+table.addEventListener("click", function (event) {
+  if (event.target && event.target.nodeName == "TD") {
+    event.target.style.backgroundColor = color.value;
+  }
+});
